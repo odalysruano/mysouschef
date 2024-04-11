7d8145fa98a044e01cf89a8d039fa73c00471f83
odalysruano <odalys.r.ruano@gmail.com>
2024-04-10 23:03:47 -0400

    add ingredients api controller

---
+++ controllers/api/ingredients.js
@@ -0,0 +1,25 @@
+const BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients";
+const rapidAPIKey = process.env.RAPID_API_KEY;
+
+async function search(req, res) {
+    const fetchOptions = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': rapidAPIKey,
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+        }
+    };
+    const url = `${BASE_URL}/search?query=${req.params.query}`;
+    try {
+        const response = await fetch(url, fetchOptions);
+        const result = await response.text();
+        res.json(result.results)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Internal Server Error');
+    }
+}
+
+module.exports = {
+    search,
+};
