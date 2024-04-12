d5574894e981a7d4190477b7b4ab41a1bb00db7b
odalysruano <odalys.r.ruano@gmail.com>
2024-04-12 14:35:52 -0400

    create user api utilities for pantry

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -12,3 +12,20 @@ export function login(credentials) {
 export function checkToken() {
     return sendRequest(`${BASE_URL}/check-token`);
 }
+
+export function getPantry() {
+    return sendRequest(`${BASE_URL}/pantry`, 'GET');
+}
+
+export function addToPantry(name, id) {
+    return sendRequest(`${BASE_URL}/pantry`, 'POST', {
+        name,
+        id,
+    });
+}
+
+export function removeFromPantry(ingredient) {
+    return sendRequest(`${BASE_URL}/pantry`, 'DELETE', {
+        ingredient
+    });
+}
