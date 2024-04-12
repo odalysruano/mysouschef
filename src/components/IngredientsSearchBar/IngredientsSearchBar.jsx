79021e201f7725ef504fec550ecdf414dc8b1958
odalysruano <odalys.r.ruano@gmail.com>
2024-04-12 16:10:21 -0400

    cleanup ingredient search bar component

--- src/components/IngredientsSearchBar/IngredientsSearchBar.jsx
+++ src/components/IngredientsSearchBar/IngredientsSearchBar.jsx
@@ -3,10 +3,13 @@ import * as ingredientsAPI from '../../utilities/ingredients-api';
 
 export default function IngredientsSearchBar({setSearchResults}) {
     const [searchQuery, setSearchQuery] = useState('');
+
     async function handleClick(evt) {
         const results = await ingredientsAPI.search(searchQuery);
         setSearchResults(results);
+        setSearchQuery('');
     };
+
     return (
         <>
             <input 
