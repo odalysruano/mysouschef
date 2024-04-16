4d4c10e94bdbcec543b8d05b456e8996293bf621
odalysruano <odalys.r.ruano@gmail.com>
2024-04-16 09:10:59 -0400

    add material-ui grid to AddMyOwnRecipe page

--- src/pages/AddMyOwnRecipePage/AddMyOwnRecipePage.jsx
+++ src/pages/AddMyOwnRecipePage/AddMyOwnRecipePage.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import IngredientCard from '../../components/IngredientCard/IngredientCard';
 import IngredientsSearchBar from '../../components/IngredientsSearchBar/IngredientsSearchBar';
 import * as RecipeAPI from '../../utilities/recipes-api';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 export default function AddToAllRecipes() {
     const [searchResults, setSearchResults] = useState([]);
@@ -33,52 +35,72 @@ export default function AddToAllRecipes() {
     }
 
     return (
-        <>
-            <h1>Add a New Recipe</h1>
-            <input 
-                type="text"
-                name="name"
-                placeholder="Recipe Name" 
-                value={recipe.name}
-                onChange={handleRecipeUpdate}
-            />
-            <input 
-                type="text"
-                name="servings"
-                placeholder="# of Servings" 
-                value={recipe.servings}
-                onChange={handleRecipeUpdate}
-            />
-            <input 
-                type="text"
-                name="instructions"
-                placeholder="Instructions" 
-                value={recipe.instructions}
-                onChange={handleRecipeUpdate}
-            />
-            <button onClick={addRecipe}>Save Recipe</button>
-            {recipe.ingredients.map((ingredient) => (
-                <IngredientCard
-                    addIngredient={addIngredient}
-                    currentIngredients={recipe.ingredients}
-                    ingredient={ingredient}
-                    key={ingredient.id}
-                    removeIngredient={removeIngredient}
-                    showButton={true}
-                />
-            ))}
-            <br></br>
-            <IngredientsSearchBar setSearchResults={setSearchResults} />
-            {searchResults.map((ingredient) => (
-                <IngredientCard
-                    addIngredient={addIngredient}
-                    currentIngredients={recipe.ingredients}
-                    ingredient={ingredient}
-                    key={ingredient.id}
-                    removeIngredient={removeIngredient}
-                    showButton={true}
-                />
-            ))}
-        </>
+        <Container>
+            <Grid container>
+                <Grid item xs={12} md={12}>
+                    <h1>Add a New Recipe</h1>
+                </Grid>
+                <Grid container xs={12} md={6}>
+                    <Grid item xs={12} md={12}>
+                        <input 
+                            type="text"
+                            name="name"
+                            placeholder="Recipe Name" 
+                            value={recipe.name}
+                            onChange={handleRecipeUpdate}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <input 
+                            type="text"
+                            name="servings"
+                            placeholder="# of Servings" 
+                            value={recipe.servings}
+                            onChange={handleRecipeUpdate}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <input 
+                            type="text"
+                            name="instructions"
+                            placeholder="Instructions" 
+                            value={recipe.instructions}
+                            onChange={handleRecipeUpdate}
+                        />
+                    </Grid>
+                    {recipe.ingredients.map((ingredient) => (
+                        <IngredientCard
+                            addIngredient={addIngredient}
+                            currentIngredients={recipe.ingredients}
+                            ingredient={ingredient}
+                            key={ingredient.id}
+                            removeIngredient={removeIngredient}
+                            showButton={true}
+                        />
+                    ))}
+                    <br></br>
+                    <Grid item xs={12} md={12}>
+                        <button onClick={addRecipe}>Save Recipe</button>
+                    </Grid>
+                </Grid>
+                <Grid container xs={12} md={6}>
+                    <Grid item xs={12} md={12}>
+                        <IngredientsSearchBar setSearchResults={setSearchResults} />
+                    </Grid>
+                    {searchResults.map((ingredient) => (
+                        <Grid item xs={12} md={12}>
+                            <IngredientCard
+                                addIngredient={addIngredient}
+                                currentIngredients={recipe.ingredients}
+                                ingredient={ingredient}
+                                key={ingredient.id}
+                                removeIngredient={removeIngredient}
+                                showButton={true}
+                            />
+                        </Grid>
+                    ))}
+                </Grid> 
+            </Grid> 
+        </Container>
     );
 }
